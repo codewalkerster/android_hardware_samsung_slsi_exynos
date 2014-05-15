f3ccbb52c6fdb428fe01b5ce293846da8979080c
codewalker <codewalker@hardkernel.com>
2014-05-15 13:50:16 +0900

    samsung patch

---
+++ libcsc/Android.mk
@@ -0,0 +1,84 @@
+LOCAL_PATH := $(call my-dir)
+
+OMX_NAME := exynos
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	csc_helper.c
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+LOCAL_C_INCLUDES := \
+	system/core/include \
+	hardware/samsung_slsi/openmax/include/khronos \
+	hardware/samsung_slsi/openmax/include/$(OMX_NAME) \
+	hardware/samsung_slsi/$(TARGET_BOARD_PLATFORM)/include \
+	frameworks/native/include/ \
+	$(LOCAL_PATH)/../include
+
+LOCAL_CFLAGS := \
+	-DUSE_SAMSUNG_COLORFORMAT \
+	-DEXYNOS_OMX
+
+LOCAL_MODULE := libcsc_helper
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := liblog
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+	csc.c
+
+LOCAL_C_INCLUDES := \
+	hardware/samsung_slsi/$(TARGET_BOARD_PLATFORM)/include \
+	hardware/samsung_slsi/openmax/include/khronos \
+	hardware/samsung_slsi/openmax/include/$(OMX_NAME) \
+	$(LOCAL_PATH)/../libexynosutils
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE := libcsc
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_STATIC_LIBRARIES := libswconverter
+LOCAL_WHOLE_STATIC_LIBRARIES := libcsc_helper
+LOCAL_SHARED_LIBRARIES := liblog libexynosutils
+
+LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/../include
+
+ifeq ($(TARGET_BOARD_PLATFORM),exynos3)
+LOCAL_CFLAGS += -DENABLE_FIMC
+LOCAL_SHARED_LIBRARIES += libexynosfimc
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
+LOCAL_CFLAGS += -DENABLE_FIMC
+LOCAL_SHARED_LIBRARIES += libexynosfimc
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),exynos5)
+LOCAL_CFLAGS += -DENABLE_GSCALER
+LOCAL_SHARED_LIBRARIES += libexynosgscaler
+ifeq ($(BOARD_USES_SCALER), true)
+LOCAL_CFLAGS += -DENABLE_SCALER
+LOCAL_SHARED_LIBRARIES += libexynosscaler
+endif
+endif
+
+LOCAL_CFLAGS += -DUSE_ION
+LOCAL_SHARED_LIBRARIES += libion_exynos
+
+LOCAL_CFLAGS += -DEXYNOS_OMX
+
+include $(BUILD_SHARED_LIBRARY)
